c03ee6bb9a8a81fb1f60c370fc0d1b764fbb8ea3
Bensome01 <minicrafter.log@gmail.com>
2025-03-14 12:36:04 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+//var tests = File.ReadAllLines("./tests.txt").ToList();
+
+Console.WriteLine(Leet.Method(1));
+
+
+static class Leet
+{
+    public static void Method()
+    {
+        //
+    }
+}
