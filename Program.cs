660f943eb93356a39cd970e30b939d0665c87f8e
Bensome01 <minicrafter.log@gmail.com>
2025-03-14 14:02:42 -0700

    solve

--- Program.cs
+++ Program.cs
@@ -4,13 +4,45 @@ using System.Text;
 
 //var tests = File.ReadAllLines("./tests.txt").ToList();
 
-Console.WriteLine(Leet.Method());
+var s = "()[]{}";
+
+Console.WriteLine(Leet.IsValid(s));
 
 
 static class Leet
 {
-    public static int Method()
-    {
-        return 1;
+    public static bool IsValid(string s) {
+        Dictionary<char, char> pairs = new Dictionary<char, char>(){
+            {'(', ')'},
+            {'[', ']'},
+            {'{', '}'}
+        };
+        HashSet<char> leftPara = new HashSet<char>() {
+            ')',
+            ']',
+            '}'
+        };
+        List<char> checker = new List<char>();
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(leftPara.Contains(s[i]))
+            {
+                if(checker.Count == 0 || pairs[checker[checker.Count - 1]] != s[i])
+                {
+                    return false;
+                }
+                else
+                {
+                    checker.RemoveAt(checker.Count - 1);
+                }
+            }
+            else
+            {
+                checker.Add(s[i]);
+            }
+        }
+
+        return checker.Count == 0;
     }
 }
