e6cd52d2b68ad7e0a7c9c17063c8b27d20254109
Bensome01 <minicrafter.log@gmail.com>
2025-03-14 13:52:38 -0700

    solve

--- Program.cs
+++ Program.cs
@@ -4,13 +4,39 @@ using System.Text;
 
 //var tests = File.ReadAllLines("./tests.txt").ToList();
 
-Console.WriteLine(Leet.Method());
+var digits = "23";
+
+Console.WriteLine(Leet.LetterCombinations(digits));
 
 
 static class Leet
 {
-    public static int Method()
+    public static IList<string> LetterCombinations(string digits) {
+        return digits.Aggregate(Enumerable.Empty<string>(),
+                (combinations, digit) => GenerateLetterCombinations(digit, combinations))
+            .ToList();
+    }
+
+    private static IEnumerable<string> GenerateLetterCombinations(
+        char num,
+        IEnumerable<string> currentCombinations)
     {
-        return 1;
+        var numToCharConverter = new Dictionary<char, char[]>
+        {
+            {'2', ['a', 'b', 'c']},
+            {'3', ['d', 'e', 'f']},
+            {'4', ['g', 'h', 'i']},
+            {'5', ['j', 'k', 'l']},
+            {'6', ['m', 'n', 'o']},
+            {'7', ['p', 'q', 'r', 's']},
+            {'8', ['t', 'u', 'v']},
+            {'9', ['w', 'x', 'y', 'z']},
+        };
+        
+        return numToCharConverter[num].Aggregate(Enumerable.Empty<string>(),
+            (current, numToChar) => current
+                .Concat(currentCombinations.Any()
+                    ? currentCombinations.Select(combination => combination + numToChar)
+                    : ["" + numToChar]));
     }
 }
