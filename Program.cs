d78c70e05490e2cb831d8d4b447566b8aa5c9ba6
Bensome01 <minicrafter.log@gmail.com>
2025-03-14 12:48:15 -0700

    solve

--- Program.cs
+++ Program.cs
@@ -4,13 +4,29 @@ using System.Text;
 
 //var tests = File.ReadAllLines("./tests.txt").ToList();
 
-Console.WriteLine(Leet.Method(1));
+var nums = new [] { 2, 7, 11, 15 };
+var target = 9;
+
+Console.WriteLine(Leet.TwoSum(nums, target));
 
 
 static class Leet
 {
-    public static void Method()
-    {
-        //
+    public static int[] TwoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        for(int i = 0; i < nums.Length; i++)
+        {
+            for(int j = i + 1; j < nums.Length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    arr[0] = i;
+                    arr[1] = j;
+                    return arr;
+                }
+            }
+        }
+
+        return arr;
     }
 }
