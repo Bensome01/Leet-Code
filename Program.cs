0c6629e6a56471474337636a97097823c661c9d6
Bensome01 <minicrafter.log@gmail.com>
2025-03-14 13:59:38 -0700

    solve

--- Program.cs
+++ Program.cs
@@ -4,13 +4,54 @@ using System.Text;
 
 //var tests = File.ReadAllLines("./tests.txt").ToList();
 
-Console.WriteLine(Leet.Method());
+var nodes = new[] { 1, 2, 3, 4 };
+
+var head = nodes.Reverse().Aggregate(new ListNode(5), (list, node) =>
+    {
+        return new ListNode(node, list);
+    });
+var n = 2;
+
+Console.WriteLine(Leet.RemoveNthFromEnd(head, n));
 
 
 static class Leet
 {
-    public static int Method()
-    {
-        return 1;
+    public static ListNode RemoveNthFromEnd(ListNode head, int n) {
+        if(head.next == null)
+        {
+            return null;
+        }
+
+        ListNode trail = head;
+        ListNode end = head.next;
+        int listLength = 2;
+        while(end.next != null)
+        {
+            end = end.next;
+            if(listLength >= n + 1)
+            {
+                trail = trail.next;
+            }
+            listLength++;
+        }
+
+        if(listLength == n)
+        {
+            return trail.next;
+        }
+
+        trail.next = trail.next.next;
+
+        return head;
     }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+         this.val = val;
+         this.next = next;
+     }
 }
