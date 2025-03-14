8471c9ffe219d542bb54ce0b83c5feffc5bbb963
Bensome01 <minicrafter.log@gmail.com>
2025-03-14 13:12:25 -0700

    solve

--- Program.cs
+++ Program.cs
@@ -4,13 +4,29 @@ using System.Text;
 
 //var tests = File.ReadAllLines("./tests.txt").ToList();
 
-Console.WriteLine(Leet.Method(1));
+var s = "PAYPALISHIRING";
+var numRows = 3;
+
+Console.WriteLine(Leet.Convert(s, numRows));
 
 
 static class Leet
 {
-    public static void Method()
-    {
-        //
+    public static string Convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        string zigzag = "";
+        int zigSize = numRows * 2 - 2;
+        int indexIncrement;
+        for (int i = 0; i < numRows; i++)
+        {
+            indexIncrement = (zigSize - i * 2) % zigSize;
+            for (int j = i; j < s.Length;)
+            {
+                zigzag += s[j];
+                indexIncrement = ((indexIncrement * (zigSize - 1)) % zigSize);
+                j += zigSize - indexIncrement;
+            }
+        }
+        return zigzag;
     }
 }
