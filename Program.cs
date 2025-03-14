c9a27da036c05662b5919c7524f7c38e220f702f
Bensome01 <minicrafter.log@gmail.com>
2025-03-14 13:17:21 -0700

    solve

--- Program.cs
+++ Program.cs
@@ -4,13 +4,51 @@ using System.Text;
 
 //var tests = File.ReadAllLines("./tests.txt").ToList();
 
-Console.WriteLine(Leet.Method(1));
+var s = "42";
+
+Console.WriteLine(Leet.MyAtoi(1));
 
 
 static class Leet
 {
-    public static void Method()
-    {
-        //
+    public static int MyAtoi(string str) {
+     
+        string temp = str;
+        temp = temp.Trim();
+        if(string.IsNullOrEmpty(temp)) return 0;
+
+        bool isNeg = false;
+        if(temp[0] == '-' || temp[0] == '+'){
+            if (temp[0] == '-'){
+                isNeg = true;
+            }
+
+            temp = temp.Remove(0,1);
+        }   
+        
+        bool hitnonzero = false;
+        StringBuilder sb = new StringBuilder();
+        for(int i =0; i< temp.Length; i++){
+            if(temp[i] == '0' && !hitnonzero){
+                    
+            }
+            else{
+                if(!char.IsDigit(temp[i])){
+                    break;
+                }
+                hitnonzero = true;
+                sb.Append(temp[i]);
+            }
+        }
+        if(string.IsNullOrEmpty(sb.ToString())) return 0;
+        
+        if(Convert.ToDouble(sb.ToString()) > int.MaxValue){
+            if(isNeg) return int.MinValue;
+            else return int.MaxValue;
+        }
+        else{
+            if(isNeg) return Convert.ToInt32(sb.ToString())*-1;
+            else return Convert.ToInt32(sb.ToString());
+        }
     }
 }
