0e53087db8be667644920744da33d87db8aa3324
Bensome01 <minicrafter.log@gmail.com>
2025-03-14 13:15:04 -0700

    solve

--- Program.cs
+++ Program.cs
@@ -4,13 +4,26 @@ using System.Text;
 
 //var tests = File.ReadAllLines("./tests.txt").ToList();
 
-Console.WriteLine(Leet.Method(1));
+var x = 123;
+
+Console.WriteLine(Leet.Reverse(1));
 
 
 static class Leet
 {
-    public static void Method()
-    {
-        //
+    public static int Reverse(int x) {
+        string s = x.ToString();
+        string reverse = "";
+        for(int i = s.Length - 1; i >= 0; i--)
+        {
+            reverse += s[i];
+        }
+        if(s[0] == '-')
+        {
+            reverse = '-' + reverse.Substring(0, reverse.Length - 1);
+        }
+        int num;
+        int.TryParse(reverse, out num);
+        return num;
     }
 }
